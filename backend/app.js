60e97faf3863f09e0fa7fb147b8dc128a910bfe0
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-11-15 19:40:45 +0300

    fix: mongoose.connect

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const { PORT = 3001 } = process.env;
 const app = express();
 
 // для подключения к БД
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 // Безопасность. Обработка CORS запросов
 const options = {
